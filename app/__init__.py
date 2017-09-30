b79cab2fcbf86931cdb54b1b7b82c8a873f238a8
MarloneA <marlone911@gmail.com>
2017-09-30 15:43:59 +0300

    perfoming migrations

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+
 
 # local imports
 from config import app_config
@@ -15,6 +17,10 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    migrate = Migrate(app, db)
+
+    from app import models
 
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
